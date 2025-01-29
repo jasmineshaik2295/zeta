80a40e753ff7a18cd533483c53aa5cbfecac3503
jasmineshaik2295 <jasmineshaik2295@gmail.com>
2025-01-29 11:55:36 +0000

    mute

---
+++ mutable.py
@@ -0,0 +1,15 @@
+list = [10, 20, 30]
+tuple = ("apple","banana","cherry")
+dict = {"name":"jasmine","age":19,"city":"nellore"}
+
+print("List Elements:")
+print("First Element:",list[0])
+print("Last Element:",list[-1])
+
+print("\nTuple Elements:")
+print("Second Element:",tuple[0])
+print("Last Element:",tuple[-1])
+
+print("\nDict Elements:")
+print("Value of key 'name':",dict["name"])
+print("Value of key 'city':",dict["city"])
